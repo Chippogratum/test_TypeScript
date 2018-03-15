7a55714ceb521c23f876788729889d368cb029ee
Chippogratum <vkajuk1@gmail.com>
2018-03-15 19:20:36 +0200

    1) создал новый проект для работы с TypeScript на VS17

---
+++ test_ts/app.js
@@ -0,0 +1,10 @@
+var User = /** @class */ (function () {
+    function User(_name, _age) {
+        this.name = _name;
+        this.age = _age;
+    }
+    return User;
+}());
+var tom = new User("Том", 29);
+console.log("Имя: ", tom.name, " возраст: ", tom.age);
+//# sourceMappingURL=app.js.map
--- test_ts/app.ts
+++ test_ts/app.ts
@@ -1,1 +1,11 @@
-﻿console.log('Hello world');
+﻿class User {
+	name: string;
+	age: number;
+	constructor(_name: string, _age: number) {
+
+		this.name = _name;
+		this.age = _age;
+	}
+}
+var tom: User = new User("Том", 29);
+console.log("Имя: ", tom.name, " возраст: ", tom.age);
